109c9a5484508cd9ff26b9b980f35fdbeaa25fd6
SoapyTarantula <soapytarantula@gmail.com>
2022-09-13 15:11:11 -0500

    Update BulletHolder.gd

--- BulletHolder.gd
+++ BulletHolder.gd
@@ -3,11 +3,12 @@ extends Node
 onready var player = get_node("/root/Main/Player")
 
 func _ready():
-	print(player)
+	pass
 
 
 func _on_Player_shoot_attempt():
 	var bullet = load("res://Projectile.tscn")
 	var b = bullet.instance()
-	var bh = get_node("BulletHolder") # figure out why the fuck this is null all the time regardless of path
+	var bh = get_node("/root/Main/BulletHolder") # SOLVED - godot wants you to say /root/Main/etc even though it knows what root/Main is, wtf
 	bh.add_child(b, true)
+	print(str(bh) + " | " + str(b))
