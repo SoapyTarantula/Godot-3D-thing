024defc67b885f65463135856544ac6cc9a9195e
SoapyTarantula <soapytarantula@gmail.com>
2022-09-13 18:59:42 -0500

    All sorts of new and exciting errors

--- BulletHolder.gd
+++ BulletHolder.gd
@@ -3,12 +3,11 @@ extends Node
 onready var player = get_node("/root/Main/Player")
 
 func _ready():
-	pass
-
+	player.connect("shoot_attempt", self, "_on_Player_shoot_attempt") # Have this script listen for the shooting signal from the player
 
 func _on_Player_shoot_attempt():
 	var bullet = load("res://Projectile.tscn")
 	var b = bullet.instance()
 	var bh = get_node("/root/Main/BulletHolder") # SOLVED - godot wants you to say /root/Main/etc even though it knows what root/Main is, wtf
 	bh.add_child(b, true)
-	print(str(bh) + " | " + str(b))
+	#b.transform.origin = player.get_node("Camera/WeaponHolder/SMG/Muzzle").transform.origin # Figure out some way of getting the bullet to the actual muzzle of the gun that doesn't throw a fucking error
--- Weapon.gd
+++ Weapon.gd
@@ -6,7 +6,7 @@ var muzzle_velocity = 20
 func _ready():
 	pass
 	
-func _on_Player_shoot_attempt():
-	var Bullet = load("res://Projectile.tscn")
-	var b = Bullet.instance()
-	b.transform.origin = $SMG/Muzzle.transform.origin
+#func _on_Player_shoot_attempt():
+#	var Bullet = load("res://Projectile.tscn")
+#	var b = Bullet.instance()
+#	b.transform.origin = $SMG/Muzzle.transform.origin
