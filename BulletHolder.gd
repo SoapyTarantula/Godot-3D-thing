efcca36cfac2fe2d1cba92ef22ca1e662f5c16ef
SoapyTarantula <soapytarantula@gmail.com>
2022-09-13 14:17:13 -0500

    Added comment to BulletHolder.gd

--- BulletHolder.gd
+++ BulletHolder.gd
@@ -6,8 +6,8 @@ func _ready():
 	print(player)
 
 
-#func _on_Player_shoot_attempt():
-	#var bullet = load("res://Projectile.tscn")
-	#var b = bullet.instance()
-	#var bh = get_node("BulletHolder")
-	#bh.add_child(b, true)
+func _on_Player_shoot_attempt():
+	var bullet = load("res://Projectile.tscn")
+	var b = bullet.instance()
+	var bh = get_node("BulletHolder") # figure out why the fuck this is null all the time regardless of path
+	bh.add_child(b, true)
